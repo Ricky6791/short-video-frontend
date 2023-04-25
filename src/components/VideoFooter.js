4f0a46b807e731ea9c3e1f82eafd233abd200144
Ricardo Avila <ricky6791@gmail.com>
2023-04-25 10:21:06 -0600

    icons

--- src/components/VideoFooter.js
+++ src/components/VideoFooter.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './VideoFooter.css'
+import MusicNoteIcon from '@material-ui/icons/MusicNote'
+import Ticker from 'react-ticker'
 
 const VideoFooter = () => {
     return (
@@ -9,8 +11,16 @@ const VideoFooter = () => {
                 <p>description</p>
                 <div className="videoFooter_ticker">
                     <MusicNoteIcon className="videoFooter_icon" />
+                    <Ticker mode="smooth">
+                        {({ index }) => (
+                            <>
+                                <p>I am a windows PC</p>
+                            </>
+                        )}
+                    </Ticker>
                 </div>
             </div>
+            <img className="videoFooter_record" src="https://static.thenounproject.com/png/934821-200.png" alt="video footer" />
         </div>
     )
 }
